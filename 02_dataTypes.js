d3a496157ddaa31b7ee8250633508f93136dcbee
Uzair <uzairkhan03010@gmail.com>
2026-07-22 15:59:02 +0500

    dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"user strict"; //treat all  code  as newer version
+
+// alert(3+3) // we are using nodejs, not brower
+
+console.log(3
+    +
+    3)  //code readability is important, so we should write code in a readable way
+
+console.log("uzair")
+
+let name = "uzair"
+let age = 21
+let isloggedIn = true
+
+//number
+// bigint
+//string =>""
+//boolean => true/false
+//null = stand alone
+//undefined 
+// symbol => unique
+//object 
+
+console.log(typeof name)
+console.log(null)
