21c8191de2d0fb7ca5aaf20d54135d2e9ac44db8
Uzair <uzairkhan03010@gmail.com>
2026-07-23 12:56:21 +0500

    camparision

---
+++ 01_basics/04_camparision.js
@@ -0,0 +1,11 @@
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >= 0);
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
+//check strickly equal
+console.log("2" === 2);
