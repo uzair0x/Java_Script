562649018eb604a9028945ee26764b37d0c2e670
Uzair <uzairkhan03010@gmail.com>
2026-07-23 12:35:54 +0500

    ConverionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,57 @@
+let score = "null89898"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN
+//true => 1; false => 0
+
+
+
+let isLoggedIn = "uziar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//"0"=> false
+//"uzair"=> true
+
+let someNumber = 34
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//******************************** Operations ****************************************//
+ 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3 + 2);
+// console.log(3 - 2);
+// console.log(3 * 2);
+// console.log(3 ** 2);
+// console.log(3 / 2);
+// console.log(3 % 2);
+
+// console.log(3 + "2"); // 32
+// console.log(3 - "2"); // 1
+// console.log(3 * "2"); // 6
+// console.log(3 / "2"); // 1.5
+// console.log(3 % "2"); // 1
+
+// console.log("1" + 2 + 3 );
+
+// console.log( (5 + 8 ) * 8 % 4 );
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++ //101
+++gameCounter//102
+console.log(gameCounter);
