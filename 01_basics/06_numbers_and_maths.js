990349f3582f0c7a057f979c83536f4e01731d0c
Uzair <uzairkhan03010@gmail.com>
2026-07-24 11:19:32 +0500

    Number and Maths

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,43 @@
+const score = 400
+//console.log(score);
+
+//const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); 
+
+const otherNumber = 35.944454
+
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds =1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//************************************Maths******************* */
+
+// console.log(Math);
+// console.log(Math.abs(-5.3));
+// console.log(Math.round(5.3));
+// console.log(Math.floor(5.9));
+// console.log(Math.ceil(5.1));
+// console.log(Math.min(5, 3, 7, 1));
+// console.log(Math.max(5, 3, 7, 1));console.log(Math);
+// console.log(Math.abs(-5.3));
+// console.log(Math.round(5.3));
+// console.log(Math.floor(5.9));
+// console.log(Math.ceil(5.1));
+// console.log(Math.min(5, 3, 7, 1));
+// console.log(Math.max(5, 3, 7, 1));
+
+
+console.log(Math.random());
+console.log((Math.random() *10) + 1);
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
