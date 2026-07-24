8d006d6eb6aac2405e3d5c239828dbb05e98aa01
Uzair <uzairkhan03010@gmail.com>
2026-07-24 12:39:40 +0500

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,34 @@
+//Array
+
+const myArr   = [0, 1, 2, 3, 4, 5]
+const myHeros = ["ironman", "thor"]
+
+const myArr2  = new Array(1, 2, 3, 4, 5)
+// console.log(myArr[3]);
+
+//Array Methods
+
+// myArr.push(6) //add to the end of the array
+// myArr.pop() //remove from the end of the array
+// console.log(myArr);
+
+// console.log(myArr.includes(7));
+
+const newArr = myArr.join()
+
+// console.log(typeof newArr);
+// console.log(typeof myArr);
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1, 3) //slice the array from index 1 to 3 (not including 3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 =  myArr.splice(1, 3) //splice the array from index 1 to 3 (including 3)
+console.log(myn2);const myn1 = myArr.slice(1, 3) //slice the array from index 1 to 3 (not including 3)
+
+console.log(myn1);
+console.log"B
+console.log(myArr);
