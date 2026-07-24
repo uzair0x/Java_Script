e681621b95184f4dd2cec1478891ccca1f86d071
Uzair <uzairkhan03010@gmail.com>
2026-07-24 12:05:10 +0500

    Dates in Js

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,23 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+let myCreatedDate = new Date(2026 , 6 , 24 )
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getFullYear());
+console.log(newDate.getDay(newDate.toString()));
+// console.log(myCreatedDate.getTime());
