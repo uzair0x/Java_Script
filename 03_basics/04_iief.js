f70a2be248112c086d84b1c64d276cd04275ee26
Uzair <uzairkhan03010@gmail.com>
2026-07-26 19:21:25 +0500

    .

---
+++ 03_basics/04_iief.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression (IIEF)
+
+(function chia(){
+    console.log(`DB  CONNECTED`);
+
+}());
+
+((name) =>{
+    console.log(`DB TWO Connected ${name}`)
+})('uzair')
