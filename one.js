cb81cd0aa851fc589d398a679687348aef08d3b8
Uzair <uzairkhan03010@gmail.com>
2026-07-27 15:46:39 +0500

    for loop

---
+++ one.js
@@ -0,0 +1,23 @@
+////////for
+// for (let i = 0; i <=10 ; i++) {
+//     const element = i
+//     if (element == 5) {
+//         console.log("5 is best number");
+//     }
+//     //console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++ ){
+//    console.log(`outer loop value : ${i}`);
+    for( let j = 0 ; j<=10 ; j++ )
+        // console.log(`Inner loop value ${j}and outer loop value ${i}`)
+   //     console.log(i + '*' + j + '=' + i*j);
+// console.log(`Inner loop value ${j}and outer loop value ${i}`)
+
+// let myArray = ["flash", "batman" , "superman"]
+// //console.log(myArray.length);
+// for (let index = 0 ; index < myArray.length; index++){
+//     const element = myArray [index];
+//     console.log(element);
+// }
+ 
