86a919b23891f21cca5917872400de8df1d73324
Uzair <uzairkhan03010@gmail.com>
2026-07-27 18:12:26 +0500

    foreach loop

---
+++ 05_iteration./five.js
@@ -0,0 +1,138 @@
+ const conding = ["js", "ruby", "java", "python", "cpp"]
+
+// conding.forEach(function (val){
+//     console.log(val);
+// })
+
+
+// conding.forEach((item)=>{
+//     console.log(item);
+// })
+
+// conding.forEach((item)=>{
+//     console.log(item);
+// })
+
+// con
+// conding.forEach(printMe)
+
+// Array
+
+const myCoding = [
+    {
+        languagesName: "javascript",
+        languageFIleName: " js"
+    },
+    {
+        languagesName: "java",
+        languageFIleName: "java"
+    },
+    {
+        languagesName: "python",
+        languageFIleName: "py"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFIleName)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
