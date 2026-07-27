10c4599330f33a63fa5b813312eaf27cc4fba9b1
Uzair <uzairkhan03010@gmail.com>
2026-07-27 11:45:28 +0500

    Control Flow

---
+++ 04_controlFlow/01_basics.js
@@ -0,0 +1,37 @@
+// //if
+// const isUserLogedIn = true
+// const temp = 37
+// if(temp === 36){
+//     console.log("less than 50")
+// }else{
+//     console.log("Temp is grater than 37")
+// }
+// < , > <= , >= ,= ,==, === , != ,!==
+
+// const  score = 200
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User powe: ${power}`);
+// }
+
+//     console.log(`User powe: ${power}`);
+
+// const  score = 1000
+
+// if(score > 500 ) console.log("Test"),console.log("Test2")
+
+const userLogedIn = true
+const debitCard  = true
+const loggedInFromGoogle = false
+const fromemail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allowed to bub courses");
+}// const  score = 1000
+
+// if(score > 500 ) console.log("Test"),console.log("Test2")
+
+
+if(loggedInFromGoogle || fromemail){
+    console.log(" user loggedIN");
+}
