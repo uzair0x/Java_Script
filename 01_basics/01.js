3390d375e6b1f5ef0f70c216b3c0cbbb60833c56
Uzair <uzairkhan03010@gmail.com>
2026-08-01 17:18:17 +0500

    .

---
+++ 01_basics/01.js
@@ -0,0 +1,21 @@
+let list = [
+    {
+        name: "Maxwell",
+        phone: "(0191)7196459",
+        email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    },
+    
+    {
+        name: "Raja Villarreal",
+        phone: "0866 398 2895",
+        email: "posuere.vulputate@sed.com"
+    },
+    
+    {
+        name: "Helen Rischards",
+        phone: "08001111",
+        email: "libero@convallis.edu"
+    }
+];
+
+console.log(list[2].email);
